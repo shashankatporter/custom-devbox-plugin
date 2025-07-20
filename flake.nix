03bbe7675cc62b42a06dde2d49ee2746e437a18a
Shashank Kumar Shukla <shashank.shukla@theporter.in>
2025-07-20 16:01:02 +0530

    feat: provide a single all-in-one plugin

--- flake.nix
+++ flake.nix
@@ -9,55 +9,46 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
       pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-      # Define the plugins data structure with paths to the scripts
-      pluginsData = {
-        org-linter = {
-          latest = ./scripts/org-linter.sh;
-        };
-        db-seeder = {
-          latest = ./scripts/db-seeder.sh;
-        };
-      };
-
       # Helper to create a package by reading a script file
       makePluginPackage = pkgs: name: scriptPath: 
         pkgs.writeShellScriptBin name (builtins.readFile scriptPath);
-      
-      # Build all packages for a given system
-      allPackagesFor = system:
-        let
-          pkgs = pkgsFor.${system};
-          makePackagesFromVersions = pluginName: versions:
-            builtins.listToAttrs (map (version: {
-              name = pluginName; # Simplified to just use the plugin name
-              value = makePluginPackage pkgs pluginName versions.${version};
-            }) (builtins.attrNames versions));
-        in
-        (makePackagesFromVersions "org-linter" pluginsData.org-linter) //
-        (makePackagesFromVersions "db-seeder" pluginsData.db-seeder);
+
+      # Build individual tool packages for a given system
+      allToolsFor = system:
+        let pkgs = pkgsFor.${system};
+        in {
+          org-linter = makePluginPackage pkgs "org-linter" ./scripts/org-linter.sh;
+          db-seeder = makePluginPackage pkgs "db-seeder" ./scripts/db-seeder.sh;
+        };
 
     in
     {
-      packages = forAllSystems allPackagesFor;
-
-      # Expose them as Devbox plugins
-      devboxPlugins = forAllSystems (system:
+      # This output contains all packages, including the new combined one
+      packages = forAllSystems (system:
         let
-          systemPackages = allPackagesFor system;
+          pkgs = pkgsFor.${system};
+          tools = allToolsFor system;
         in
-        {
-          # The linter just provides a ready message
-          org-linter = {
-            package = systemPackages.org-linter;
-            init_hook = "echo '✅ Porter org-linter ready! Run org-linter to use.'";
-          };
-
-          # The seeder will run automatically every time you start the shell
-          db-seeder = {
-            package = systemPackages.db-seeder;
-            init_hook = "db-seeder"; # This calls the script!
+        tools // {
+          # NEW: Create a single package that combines both tools.
+          # pkgs.symlinkJoin merges the /bin directories of all listed packages.
+          porter-tools = pkgs.symlinkJoin {
+            name = "porter-tools";
+            paths = [ tools.org-linter tools.db-seeder ];
           };
         }
       );
+
+      # This output now provides a single, default plugin for Devbox
+      devboxPlugins = forAllSystems (system: {
+        # NEW: a "default" plugin.
+        # Devbox uses this automatically when you add the flake URL without a #fragment.
+        default = {
+          # It provides the combined "porter-tools" package...
+          package = self.packages.${system}.porter-tools;
+          # ...and runs the desired init_hook.
+          init_hook = "db-seeder";
+        };
+      });
     };
 }
