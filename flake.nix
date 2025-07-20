ea85d482f5770737dcf39aa5dd4adee22e6e8ba9
Shashank Kumar Shukla <shashank.shukla@theporter.in>
2025-07-20 23:11:19 +0530

    retest the flake

--- flake.nix
+++ flake.nix
@@ -1,52 +1,26 @@
 {
-  description = "Porter Custom Devbox Plugins";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  outputs = { self, nixpkgs }:
+  description = "My Plugin-Like Devbox Flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+  outputs = { self, nixpkgs }: 
     let
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-      pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-      makePluginPackage = pkgs: name: scriptPath:
-        pkgs.writeShellScriptBin name (builtins.readFile scriptPath);
-
-      allToolsFor = system:
-        let pkgs = pkgsFor.${system};
-        in {
-          org-linter = makePluginPackage pkgs "org-linter" ./scripts/org-linter.sh;
-          db-seeder = makePluginPackage pkgs "db-seeder" ./scripts/db-seeder.sh;
-        };
-
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = pkgsFor.${system};
-          tools = allToolsFor system;
-          porter-tools-pkg = pkgs.symlinkJoin {
-            name = "porter-tools";
-            paths = [ tools.org-linter tools.db-seeder ];
-          };
-        in
-        # CORRECTED: Define startup-script at the top level of this set.
-        {
-          inherit (tools) org-linter db-seeder; # Keep these individual packages
-          porter-tools = porter-tools-pkg;
-          default = porter-tools-pkg;
-
-          startup-script = pkgs.writeTextFile {
-            name = "porter-startup.sh";
-            text = ''
-              #!/bin/sh
-              echo "--- Running Porter startup scripts ---"
-              db-seeder
-              org-linter
-              echo "--- Startup scripts complete ---"
-            '';
-          };
-        }
-      );
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        # Use overlays here if needed
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        # Packages like Devbox plugins would add
+        packages = [ pkgs.nodejs pkgs.mongodb ];
+        # Environment variables
+        shellHook = ''
+          export MY_PLUGIN_VAR=1
+          # Run custom lifecycle scripts, copy config, etc.
+          source ./scripts/my-hook.sh
+        '';
+        # Can create/copy files if desired (using hooks)
+      };
     };
 }
