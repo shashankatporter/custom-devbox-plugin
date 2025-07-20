d5cab9c4038ab041fb2e01ac79eeeb6f937d71ff
Shashank Kumar Shukla <shashank.shukla@theporter.in>
2025-07-20 13:23:44 +0530

    Fix flake outputs for multi-system support

--- flake.nix
+++ flake.nix
@@ -5,31 +5,37 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
-      # bundle the bash script together with its runtime dependencies
-      lintApp = pkgs.writeShellApplication {
-        name          = "linter";
-        runtimeInputs = [
-          pkgs.shellcheck
-          # pkgs.python3  (uncomment if your script uses Python)
-          # pkgs.nodejs   (if your script uses Node.js)
-          # ...any others you use inside the script
-        ];
-        text = builtins.readFile ./scripts/linter.sh;
-      };
+      # Support multiple systems
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in {
-      packages.${system}.linter = lintApp;
-      apps.${system}.linter = {
-        type = "app";
-        program = "${lintApp}/bin/linter";
-      };
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          
+          lintApp = pkgs.writeShellApplication {
+            name = "linter";
+            runtimeInputs = [
+              pkgs.shellcheck
+              # Add other dependencies as needed
+            ];
+            text = builtins.readFile ./scripts/linter.sh;
+          };
+        in {
+          linter = lintApp;
+          default = lintApp;  # Makes it accessible as #default too
+        });
 
-      # For backward compatibility, you can add 'default' if you want
-      # defaultPackage = myLintApp;
-      # defaultApp = {
-      #   type = "app";
-      #   program = "${myLintApp}/bin/my-lint";
-      # };
+      apps = forAllSystems (system: {
+        linter = {
+          type = "app";
+          program = "${self.packages.${system}.linter}/bin/linter";
+        };
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.linter}/bin/linter";
+        };
+      });
     };
 }
